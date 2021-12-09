c9979a5462d124784b683ef08304f46ad60fcf2f
Esteban Auguadra <Eauguadra@gmail.com>
2021-12-09 17:01:38 -0300

    add new js file with items from array

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,28 @@
+const products = [
+  { id: 1, name: "French Fries", price: "$2", stock: 10 },
+  { id: 2, name: "Soda", price: "$1.5", stock: 0 },
+  { id: 3, name: "Cheese Burger", price: "$3", stock: 5 },
+];
+
+const ItemListContainer = ({ props }) => {
+  return (
+    <div id="products-container">
+      <h1 id="products-title">Our Stock Products</h1>
+      <div id="card-container">
+        {products.map((product) => {
+          return (
+            <>
+              <div id="card">
+                <h3 id="card-title">{product.name}</h3>
+                <h4 className="card-info">Price: {product.price}</h4>
+                <h4 className="card-info">Stock: {product.stock} units</h4>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ItemListContainer;
