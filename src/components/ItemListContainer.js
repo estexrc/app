286f6f61e4c9913bb31481550c21af6aef0c733d
Esteban Auguadra <Eauguadra@gmail.com>
2021-12-14 20:16:36 -0300

    onAdd is declared

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,4 +1,5 @@
 import ItemCount from "./ItemCountFile/ItemCount";
+import Swal from "sweetalert2";
 
 const products = [
   { id: 1, name: "French Fries", price: "$2", stock: 10 },
@@ -12,12 +13,21 @@ const ItemListContainer = ({ props }) => {
       <h1 id="products-title">Our Stock Products</h1>
       <div id="card-container">
         {products.map((product) => {
+          const onAdd = () => {
+            if (product.stock <= 0) {
+            } else {
+              Swal.fire({
+                icon: "success",
+                text: "Successfully added to cart",
+              });
+            }
+          };
           return (
             <>
               <div id="card">
                 <h3 id="card-title">{product.name}</h3>
                 <h4 className="card-info">Price: {product.price}</h4>
-                <ItemCount stock={product.stock} initial={1} />
+                <ItemCount stock={product.stock} initial={1} onAdd={onAdd} />
               </div>
             </>
           );
