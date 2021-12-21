1994793cdcd567157d27d9bece2e4420c448f821
Esteban Auguadra <Eauguadra@gmail.com>
2021-12-21 12:25:59 -0300

    add buttons in detail

--- src/components/itemDetail/ItemDetail.js
+++ src/components/itemDetail/ItemDetail.js
@@ -1,6 +1,14 @@
 import React from "react";
+import ItemCount from "../ItemCountFile/ItemCount";
+import Swal from "sweetalert2";
 
 const ItemDetail = ({ description }) => {
+  const onAdd = () => {
+    Swal.fire({
+      icon: "success",
+      text: "Successfully added to cart",
+    });
+  };
   return (
     <>
       <div id="card">
@@ -13,6 +21,7 @@ const ItemDetail = ({ description }) => {
           <h4 id="card-title">{description.title}</h4>
           <h5 className="card-info">Price: ${description.price}</h5>
           <h5 className="card-info">{description.description}</h5>
+          <ItemCount stock={5} initial={1} onAdd={onAdd} />
         </div>
       </div>
     </>
