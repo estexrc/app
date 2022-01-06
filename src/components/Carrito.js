21b3d174147cf5638d86159f8b456bff34f2e1df
Esteban Auguadra <Eauguadra@gmail.com>
2022-01-05 22:06:23 -0300

    add stylus

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -5,24 +5,28 @@ const Carrito = () => {
 
   return (
     <>
-      <div id="card">
-        {cart.map((desc) => {
-          return (
-            <div className="card-container">
-              <img src={desc.img} alt="description" className="product-img" />
-              <div className="info-container ">
-                <h4 id="card-title">{desc.nombre}</h4>
-                <h5 className="card-info">
-                  Precio: $ {`${desc.precio}*${cantidad}`}
-                </h5>
-                <h5 className="card-info">{desc.descripcion}</h5>
-                <button onClick={deleteCart}>-</button>
+      <div id="cartContainer">
+        <div id="card">
+          {cart.map((desc) => {
+            return (
+              <div className="card-container">
+                <img src={desc.img} alt="description" className="product-img" />
+                <div className="info-container ">
+                  <h4 id="card-title">{desc.nombre}</h4>
+                  <h5 className="card-info">
+                    Precio: $ {`${desc.precio}*${cantidad}`}
+                  </h5>
+                  <h5 className="card-info">{desc.descripcion}</h5>
+                  <button onClick={deleteCart}>-</button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <button onClick={clearCart} className="bnt-Cart">
+          Clear
+        </button>
       </div>
-      <button onClick={clearCart}>Clear</button>
     </>
   );
 };
