8eb5c7458c0e6f6955f534148d6eb19baf757858
Esteban Auguadra <Eauguadra@gmail.com>
2022-01-14 17:44:22 -0300

    add and style circle of cart number

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -23,7 +23,7 @@ const Navbar = ({ links }) => {
         <NavLink to={"/carrito"}>
           <CartWidget />
         </NavLink>
-        {totalCart}
+        {totalCart > 0 ? <div id="cartNumber"> {totalCart}</div> : <div></div>}
       </nav>
     </header>
   );
