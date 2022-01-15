ceaf295326f9dee67fd0fa6f922c79e99e21293c
Esteban Auguadra <Eauguadra@gmail.com>
2022-01-15 13:02:44 -0300

    change the source of prod stock

--- src/components/itemDetail/ItemDetail.js
+++ src/components/itemDetail/ItemDetail.js
@@ -23,10 +23,10 @@ const ItemDetail = ({ description }) => {
             className="product-img"
           />
           <div className="info-container ">
-            <h4 id="card-title">{description.nombre}</h4>
-            <h5 className="card-info">Precio: ${description.precio}</h5>
+            <h4 id="card-title">{description.name}</h4>
+            <h5 className="card-info">Precio: ${description.price}</h5>
             <h5 className="card-info">{description.descripcion}</h5>
-            <ItemCount stock={5} initial={1} onAdd={onAdd} />
+            <ItemCount stock={description.stock} initial={1} onAdd={onAdd} />
           </div>
         </div>
       </div>
