2d7d9925b3fe55ea6fbeef56c993019250371081
Esteban Auguadra <Eauguadra@gmail.com>
2022-01-15 13:55:49 -0300

    add function addDoc for send info to firebase

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,5 +1,38 @@
+import {
+  collection,
+  getFirestore,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { useContexto } from "../myContext";
+
 const Payment = () => {
-  return <h2>Aca va a haber un formulario de pago</h2>;
+  const { cart } = useContexto();
+  const sendOrder = () => {
+    const order = {
+      buyer: {
+        name: "Esteban",
+        phone: "3413549436",
+        email: "eauguadra@gmail.com",
+      },
+      items: { ...cart },
+      date: serverTimestamp(),
+      total: 100,
+    };
+
+    const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order);
+
+    console.log("compra realizada");
+  };
+
+  return (
+    <>
+      <p>Aca se van a mostrar inputs para poner datos del cliente</p>
+      <button onClick={sendOrder}>Finalizar compra</button>
+    </>
+  );
 };
 
 export default Payment;
