af5be11aa8089e8075367a4e9326db1964add5b9
Esteban Auguadra <Eauguadra@gmail.com>
2022-01-19 11:51:06 -0300

    add img and create home links for navigate products

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1>Hola soy el home</h1>
+      <h1 className="titleForContainers">Welcome to My Store!!</h1>
+      <p className="homeSubtitle">
+        Please feel free to browser into our categories or start directly with
+        all the products
+      </p>
+      <Link to={"/products"}>
+        <div id="catalogImg" className="homeCardsImg">
+          <img
+            src="/assets/linkImg/all.jpg"
+            alt="all prod img"
+            className="catImg"
+          />
+          <p className="catText">All Products</p>
+        </div>
+      </Link>
+
+      <div id="categoriesContainer">
+        <Link to={"category/men-clothing"}>
+          <div className="homeCardsImg">
+            <img
+              src="/assets/linkImg/MC.jpg"
+              alt="men clothing img"
+              className="catImg"
+            />
+            <p className="catText">Men Clothing</p>
+          </div>
+        </Link>
+        <Link to={"category/woman-clothing"}>
+          <div className="homeCardsImg">
+            <img
+              src="/assets/linkImg/WC.jpg"
+              alt="woman clothing img"
+              className="catImg"
+            />
+            <p className="catText">Woman Clothing</p>
+          </div>
+        </Link>
+        <Link to={"category/electronics"}>
+          <div className="homeCardsImg">
+            <img
+              src="/assets/linkImg/EL.jpg"
+              alt="electronic img"
+              className="catImg"
+            />
+            <p className="catText">Electronics</p>
+          </div>
+        </Link>
+        <Link to={"category/jewelry"}>
+          <div className="homeCardsImg">
+            <img
+              src="/assets/linkImg/JE.jpg"
+              alt="jewelry img"
+              className="catImg"
+            />
+            <p className="catText">Jewelry</p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
